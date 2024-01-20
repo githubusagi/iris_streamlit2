0a57c1ba6a9c93d63f5cde794f5c0b76b27fad12
githubusagi <157009441+githubusagi@users.noreply.github.com>
2024-01-20 20:19:04 +0900

    Update iris.py

--- iris.py
+++ iris.py
@@ -2,8 +2,10 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.datasets import load_iris
+from scikit-learn
+.linear_model import LogisticRegression
+from scikit-learn
+.datasets import load_iris
 
 # データセット読み込み
 iris = load_iris()
